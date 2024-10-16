a526c6b996e184091e2c0e3a54eb9d9450b7ebcb
hakureirukoto <hakureirukoto@hotmail.com>
2024-10-16 23:44:47 +0800

    composer

--- composer/autoload_namespaces.php
+++ composer/autoload_namespaces.php
@@ -12,11 +12,10 @@ return array(
     'Mail' => array($vendorDir . '/pear/mail', $vendorDir . '/pear/mail_mime'),
     'Liuggio' => array($vendorDir . '/liuggio/statsd-php-client/src'),
     'Less' => array($vendorDir . '/wikimedia/less.php/lib'),
-    'JsonMapper' => array($vendorDir . '/netresearch/jsonmapper/src'),
     'Console' => array($vendorDir . '/pear/console_getopt'),
     'ComposerVendorHtaccessCreator' => array($baseDir . '/includes/composer'),
     'ComposerPhpunitXmlCoverageEdit' => array($baseDir . '/includes/composer'),
     'ComposerHookHandler' => array($baseDir . '/includes/composer'),
     'CSSMin' => array($vendorDir . '/wikimedia/minify/src'),
-    '' => array($vendorDir . '/cssjanus/cssjanus/src', $vendorDir . '/pear/pear-core-minimal/src'),
+    '' => array($vendorDir . '/cssjanus/cssjanus/src'),
 );
